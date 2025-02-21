86b832204cf0566453cb04af858d92a3bba0692e
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2025-02-21 17:19:53 -0300

    feature/Responsividade visual

--- README.md
+++ README.md
@@ -1,50 +1,18 @@
-# React + TypeScript + Vite
+# Meu Portfólio
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+![Logo ou Foto do Projeto](projeto.png)
 
-Currently, two official plugins are available:
+## Sobre o Projeto
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Fiz a criação do meu portfólio para demonstrar as minhas habilidades com a programação ultilizando projetos com linguagens expecíficas em front-end.
 
-## Expanding the ESLint configuration
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+## Tecnologias Utilizadas
 
-- Configure the top-level `parserOptions` property like this:
+- React
+- Tailwind
+- Typescript
 
-```js
-export default tseslint.config({
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
-})
-```
+### Visualização
 
-- Replace `tseslint.configs.recommended` to `tseslint.configs.recommendedTypeChecked` or `tseslint.configs.strictTypeChecked`
-- Optionally add `...tseslint.configs.stylisticTypeChecked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and update the config:
-
-```js
-// eslint.config.js
-import react from 'eslint-plugin-react'
-
-export default tseslint.config({
-  // Set the react version
-  settings: { react: { version: '18.3' } },
-  plugins: {
-    // Add the react plugin
-    react,
-  },
-  rules: {
-    // other rules...
-    // Enable its recommended rules
-    ...react.configs.recommended.rules,
-    ...react.configs['jsx-runtime'].rules,
-  },
-})
-```
+  Para a visualização do meu portfólio basta clicar [aqui](https://my-portfolio-lucasdelacerdas-projects.vercel.app/)
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,59 +1,59 @@
 
 export default function Projects() {
     return (
-        <section className='flex flex-col items-center bg-Gray-200  h-[160vh]'>
-            <h3 className="text-Red font-light text-2xl mt-20 inconsolata">Meu trabalho</h3>
-            <h2 className="font-semibold text-White text-3xl asap">Veja meus Projetos em destaque</h2>
-            <div className="flex flex-wrap justify-center  w-[80vw] mt-24 gap-6  ">
-                <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 w-1/2 max-w-[508px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
-                    <img className="rounded-lg max-w-[224px] max-h-[156px]" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
+        <section className='flex flex-col items-center bg-Gray-200  pb-[80px]'>
+            <h3 className="text-Red font-light text-xl md:text-2xl mt-8 md:mt-32 inconsolata">Meu trabalho</h3>
+            <h2 className="font-semibold text-White text-2xl md:text-3xl asap w-[60vw]">Veja meus Projetos em destaque</h2>
+            <div className="flex flex-col md:flex-row md:flex-wrap justify-center items-center w-[98vw] md:w-[80vw] mt-24 gap-6">
+                <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 flex-col w-3/4 max-lg:max-w-[250px] lg:flex-row max-w-[468px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
+                    <img className="rounded-lg max-w-[64vw] md:max-w-[220px] max-h-[156px] ml-1 md:m-0" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
                     <div className='flex flex-col items-start'>
                         <h3 className="font-bold text-Gray-600 asap text-lg">Titulo do projeto</h3>
                         <p className="font-thin text-Gray-500 maven text-sm">Descrição do projeto</p>
                         {/* <ul>Linguagens dos projetos individuais</ul> */}
                     </div>
                 </a>
-                <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 w-1/2 max-w-[508px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
-                    <img className="rounded-lg max-w-[224px] max-h-[156px]" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
+                 <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 flex-col w-3/4 max-lg:max-w-[250px] lg:flex-row max-w-[468px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
+                    <img className="rounded-lg max-w-[64vw] md:max-w-[220px] max-h-[156px] ml-1 md:m-0" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
                     <div className='flex flex-col items-start'>
                         <h3 className="font-bold text-Gray-600 asap text-lg">Titulo do projeto</h3>
                         <p className="font-thin text-Gray-500 maven text-sm">Descrição do projeto</p>
                         {/* <ul>Linguagens dos projetos individuais</ul> */}
                     </div>
                 </a>
-                <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 w-1/2 max-w-[508px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
-                    <img className="rounded-lg max-w-[224px] max-h-[156px]" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
+                 <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 flex-col w-3/4 max-lg:max-w-[250px] lg:flex-row max-w-[468px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
+                    <img className="rounded-lg max-w-[64vw] md:max-w-[220px] max-h-[156px] ml-1 md:m-0" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
                     <div className='flex flex-col items-start'>
                         <h3 className="font-bold text-Gray-600 asap text-lg">Titulo do projeto</h3>
                         <p className="font-thin text-Gray-500 maven text-sm">Descrição do projeto</p>
                         {/* <ul>Linguagens dos projetos individuais</ul> */}
                     </div>
                 </a>
-                <a className=" cursor-pointer flex p-4  p-3 bg-Gray-300 w-1/2 max-w-[508px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
-                    <img className="rounded-lg max-w-[224px] max-h-[156px]" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
+                 <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 flex-col w-3/4 max-lg:max-w-[250px] lg:flex-row max-w-[468px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
+                    <img className="rounded-lg max-w-[64vw] md:max-w-[220px] max-h-[156px] ml-1 md:m-0" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
                     <div className='flex flex-col items-start'>
                         <h3 className="font-bold text-Gray-600 asap text-lg">Titulo do projeto</h3>
                         <p className="font-thin text-Gray-500 maven text-sm">Descrição do projeto</p>
                         {/* <ul>Linguagens dos projetos individuais</ul> */}
                     </div>
                 </a>
-                <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 w-1/2 max-w-[508px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
-                    <img className="rounded-lg max-w-[224px] max-h-[156px]" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
+                 <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 flex-col w-3/4 max-lg:max-w-[250px] lg:flex-row max-w-[468px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
+                    <img className="rounded-lg max-w-[64vw] md:max-w-[220px] max-h-[156px] ml-1 md:m-0" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
                     <div className='flex flex-col items-start'>
                         <h3 className="font-bold text-Gray-600 asap text-lg">Titulo do projeto</h3>
                         <p className="font-thin text-Gray-500 maven text-sm">Descrição do projeto</p>
                         {/* <ul>Linguagens dos projetos individuais</ul> */}
                     </div>
                 </a>
-                <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 w-1/2 max-w-[508px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
-                    <img className="rounded-lg max-w-[224px] max-h-[156px]" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
+                 <a className="cursor-pointer flex p-4  p-3 bg-Gray-300 flex-col w-3/4 max-lg:max-w-[250px] lg:flex-row max-w-[468px] gap-3 rounded-xl hover:transform hover:scale-105 duration-300">
+                    <img className="rounded-lg max-w-[64vw] md:max-w-[220px] max-h-[156px] ml-1 md:m-0" src="./images/desenvolvimento.png" alt="Em desenvolvimento" />
                     <div className='flex flex-col items-start'>
                         <h3 className="font-bold text-Gray-600 asap text-lg">Titulo do projeto</h3>
                         <p className="font-thin text-Gray-500 maven text-sm">Descrição do projeto</p>
                         {/* <ul>Linguagens dos projetos individuais</ul> */}
                     </div>
                 </a>
-
+              
 
             </div>
         </section>
